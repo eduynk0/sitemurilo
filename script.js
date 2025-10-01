f1ace9e78638d829533d2093f1e98e8d2e25a7b3
eduynk0 <eduynk0@gmail.com>
2025-10-01 10:41:39 -0300

    git push

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let imagens = ["img/feraligatr.jpg", "img/downloand.jpg"];
+let imagens = ["img/feraligatr.jpg", "img/download.png","img/aa.png"];
 let indice = 0;
 let slide = document.getElementById("slide");
+const meuBotao = document.getElementById('btnferaligatr');
+const meuBotao2 = document.getElementById('btnblaziken');
 
 setInterval(() => {
-   indice++;
-   if (indice >= imagens.length) indice=0
-   slide.src = imagens [indice];
+    indice++
+    if (indice >= imagens.length){indice=0;}
+    slide.src = imagens [indice];
 }, 3000);
+
